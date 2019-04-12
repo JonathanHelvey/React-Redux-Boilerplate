12c48c8a2df6010ed3db5f5ac55c7b1a2fcbd9ad
JonathanHelvey <jonathanhelvey27@gmail.com>
2019-04-11 23:57:46 -0500

    jons first commit

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import UserList from '../containers/user-list';
-import UserDetails from '../containers/user-detail';
-require('../../scss/style.scss');
+import React from "react";
+require("../../scss/style.scss");
 
 const App = () => (
-    <div>
-        <h2>User List</h2>
-        <UserList />
-        <hr />
-        <h2>User Details</h2>
-        <UserDetails />
-    </div>
+  <div>
+    <h2>User Names</h2>
+    <ul>
+      <h2>User Details</h2>
+    </ul>
+  </div>
 );
 
 export default App;
